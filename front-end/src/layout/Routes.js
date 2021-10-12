0db88290bdccc1dcf7154f7bd9fc978daec48a67
Donald Farrar <donfar09@gmail.com>
2021-10-12 10:14:13 -0700

    added a route in the layout/routes folder for the NewTable Page

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NewReservation from "../reservations/NewReservation";
+import NewTable from "../tables/NewTable";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
@@ -31,6 +32,9 @@ function Routes() {
       </Route>
       <Route path="/reservations/new">
         <NewReservation />
+      </Route>
+      <Route>
+        <NewTable path="/tables/new" />
       </Route>
       <Route>
         <NotFound />
