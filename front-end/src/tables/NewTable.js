7c009630e32fbcf5fe862f1d837f10061f4f4466
Donald Farrar <donfar09@gmail.com>
2021-10-12 10:09:16 -0700

    skeleton structure of the NewTable page

--- front-end/src/tables/NewTable.js
+++ front-end/src/tables/NewTable.js
@@ -1,5 +1,26 @@
-// import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import ErrorAlert from "../layout/ErrorAlert";
 
-// export default function NewTable() {
+export default function NewTable() {
+  const history = useHistory();
+  const [error, serError] = useState([]);
+  const [formData, setFormData] = useState({ table_name: "", capacity: 1 });
 
-// }
+  return (
+    <form>
+      <ErrorAlert error={error} />
+      <label htmlFor="table_name">Table Name:&nbsp;</label>
+      <input
+        name="table_name"
+        id="table_name"
+        type="text"
+        minLength="2"
+      ></input>
+      <label htmlFor="capacity">Capacity:&nbsp;</label>
+      <input name="capacity" id="capacity" type="number" min="1"></input>
+      <button type="submit" onClick={}>Submit</button>
+      <button type="button" onClick={}>Cancel</button>
+    </form>
+  );
+}
